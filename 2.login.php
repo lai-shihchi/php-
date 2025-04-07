8a5e9b849ed61e517a8fcf796b0fbef629491ed7
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-07 22:56:23 +0800

    2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,8 @@
 <?php 
+    #輸出在 2.login.html 中輸入的帳號（來自 name="id" 的欄位）
     echo $_POST["id"];
+    #換行標籤 帳號與密碼分開顯示在不同的行
     echo "<br>";
+    #輸出在 2.login.html 中輸入的密碼（來自 name="pwd" 的欄位）
     echo $_POST["pwd"];
 ?>
