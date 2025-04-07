193e6eda217871088a99cc256dcf55001be47881
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-07 23:56:02 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,6 +1,9 @@
 <?php
+    #error_reporting(0)  取消錯誤或警告
     error_reporting(0);
+    #建立資料庫連結（ip、帳號、密碼、資料庫名稱）
     $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+    #
     $result=mysqli_query($conn, "select * from bulletin");
     echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
     while ($row=mysqli_fetch_array($result)){
