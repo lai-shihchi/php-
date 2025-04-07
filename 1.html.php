acdfa3fbd8c6a53788b14cf5ec782ee12caff7a3
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-07 21:28:39 +0800

    1.html.php

---
+++ 1.html.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>我的第一個php程式</title>
+    </head>      
+    <body>  
+        <h1>php程式</h1>
+        <h2>
+        <?php 
+            #echo為列印 輸出文字"Hello world!"
+            echo "Hello world!"; 
+        ?> 
+        </h2>
+    </body>
+</html>
