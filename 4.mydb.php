517792bee3262771ccae3bcb272941af8334e98f
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-07 23:44:56 +0800

    Update 4.mydb.php

--- 4.mydb.php
+++ 4.mydb.php
@@ -5,7 +5,8 @@
     $result=mysqli_query($conn, "select * from user");
     ##mysqli_fetch(查詢結果)
     #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-    $row=mysqli_fetch_array($result);#抓一筆資料
+    $row=mysqli_fetch_array($result);#抓取每個欄位資料 抓一筆資料 
+    #$row["欄位名稱"]
     echo $row["id"] . " " . $row["pwd"]."<br>"; 
     $row=mysqli_fetch_array($result);#抓一筆資料
     echo $row["id"] . " " . $row["pwd"];
