bf6dae3d3d06504301996d53858572a42b930115
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-08 11:31:18 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,11 +1,11 @@
 <?php
     //讀取/操作session變數之前，需呼叫session_start()，才能夠跨網頁
     session_start();
-    /*
-    刪除Session變數
+    
+    /* 刪除Session變數
     unset($_SESSION['變數名稱'])
-    session_unset() 或者session_destroy()也可以達成同樣功能
-    */
+    session_unset() 或者session_destroy()也可以達成同樣功能 */
+
     //清除 session為 "counter" 的變數
     unset($_SESSION["counter"]);
     //顯示 "counter" 已重置成功
