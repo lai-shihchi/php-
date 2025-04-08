e61b72fb1a18bce278502b270fda2ddb73dfffc4
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-08 10:35:31 +0800

    Update 10.login.php

--- 10.login.php
+++ 10.login.php
@@ -1,7 +1,7 @@
 ?php
    //mysqli_connect() 建立資料庫連結（ip、使用者名稱、密碼、資料庫名稱）
    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-   //mysqli_query() 從資料庫查詢資料
+   //mysqli_query() 查詢資料庫從 user 資料表中選取資料
    $result=mysqli_query($conn, "select * from user");
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
    $login=FALSE;
