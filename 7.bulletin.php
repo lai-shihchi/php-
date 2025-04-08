b517f7c5f1fac9bb50ed9d1265adfe4d17d0e247
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-08 11:24:12 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -5,18 +5,24 @@
     $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
     //查詢資料庫從 bulletin 資料表中選取資料
     $result=mysqli_query($conn, "select * from bulletin");
-    //建立表格標題列
+    //建立表格標題列和標題名稱
     echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
+    //使用 while 迴圈 列印出查詢結果的每一筆資料
     while ($row=mysqli_fetch_array($result)){
         //一筆一筆抓出資料並建立表格
+        // 顯示佈告編號
         echo "<tr><td>";
         echo $row["bid"];
+         // 顯示佈告類別
         echo "</td><td>";
         echo $row["type"];
+        // 顯示佈告標題
         echo "</td><td>"; 
         echo $row["title"];
+        // 顯示佈告內容
         echo "</td><td>";
         echo $row["content"]; 
+        // 顯示發佈時間
         echo "</td><td>";
         echo $row["time"];
         echo "</td></tr>";
