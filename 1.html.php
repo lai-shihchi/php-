3a1773190048fe506f522fadc55c5a8512f7c693
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-04-08 10:05:30 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -3,7 +3,7 @@
         <title>我的第一個php程式</title> #瀏覽器分頁上顯示的標題
     </head>      
     <body>  
-        <h1>php程式</h1> //html的內文
+        <h1>php程式</h1> 
         <h2>
         <?php 
             #echo為列印 輸出文字"Hello world!"
