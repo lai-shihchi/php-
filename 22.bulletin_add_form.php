56eb1cbfa3f932df1e8206073b4e1eb419f794b3
lai-shihchi <169122410+lai-shihchi@users.noreply.github.com>
2025-06-03 10:19:57 +0800

    22.bulletin_add_form.php

---
+++ 22.bulletin_add_form.php
@@ -0,0 +1,27 @@
+<?php
+    error_reporting(0); 
+    session_start(); 
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號"; 
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{
+        // 如果已登入，輸出 HTML 表單供使用者新增佈告
+        echo "
+        <html>
+            <head><title>新增佈告</title></head>
+            <body>
+                <form method=post action=23.bulletin_add.php>   //表單送出至 23.bulletin_add.php
+                    標    題：<input type=text name=title><br>  //輸入欄位：標題
+                    內    容：<br><textarea name=content rows=20 cols=20></textarea><br>  //多行輸入：內容
+                    佈告類型：<input type=radio name=type value=1>系上公告   //使用單選佈告類型
+                            <input type=radio name=type value=2>獲獎資訊
+                            <input type=radio name=type value=3>徵才資訊<br>
+                    發布時間：<input type=date name=time><p></p>  //日期：發布時間
+                    <input type=submit value=新增佈告> <input type=reset value=清除>  //提交按鈕和清除按鈕
+                </form>
+            </body>
+        </html>
+        ";
+    }
+?>
